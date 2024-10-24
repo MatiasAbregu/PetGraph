94c7a692f3e08fce30456fa5fec6452946ba2438
MatiasAbregu <matiasabregu506@gmail.com>
2024-10-24 11:58:25 -0300

    Creando temas (fondo) de juego

--- PetGraph/Form1.Designer.cs
+++ PetGraph/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(278, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 77);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PethGraph";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(636, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Color de juego";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::PetGraph.Properties.Resources.sungif;
+            this.pictureBox1.Location = new System.Drawing.Point(677, 371);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PetGraph/Form1.cs
+++ PetGraph/Form1.cs
@@ -12,9 +12,32 @@ namespace PetGraph
 {
     public partial class Form1 : Form
     {
+        private bool tema = true;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (tema)
+            {
+                pictureBox1.Image = Properties.Resources.moongif;
+                this.BackColor = Color.White;
+                tema = false;
+
+                label1.ForeColor = Color.FromArgb(30, 30, 30);
+                label2.ForeColor = Color.FromArgb(30, 30, 30);
+            } else
+            {
+                pictureBox1.Image = Properties.Resources.sungif;
+                this.BackColor = Color.FromArgb(30,30,30);
+                tema = true;
+
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+            }
         }
     }
 }
